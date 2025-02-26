5b950d7b7778aaeaa7c86b4fae4a123c8db26c80
Gabriel <gabrielpdiniz02@gmail.com>
2025-02-26 20:31:30 -0300

    pull

---
+++ OneDrive/Área de Trabalho/todosimple/src/main/java/com/gabrielpdiniz/todosimple/models/User.java
@@ -0,0 +1,41 @@
+package com.gabrielpdiniz.todosimple.models;
+
+import java.util.*;
+import javax.validation.constraints.NotEmpty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+public class User {
+    public static final String TABLE_NAME = "user";
+    public interface CreateUser {} 
+    public interface UpdateUser {}
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min = 2, max = 100)
+    private String username;
+    //Em um update de senha, tem que validar tudo primeiro
+
+    @Column(name = "password", length = 60, nullable = false)
+    @NotNull(groups = {CreateUser.class, UpdateUser.class})
+    @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
+    private String password;
+
+    //private List<Task> tasks = new ArrayList<Task>();
+    
+}
